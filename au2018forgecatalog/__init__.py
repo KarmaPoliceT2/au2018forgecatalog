290764a23f950c11d8bb21f6d1b702a034dd800d
Chris Gill <chris.gill@live.com>
2019-04-25 00:10:38 -0700

    Heroku Fixups

--- au2018forgecatalog/__init__.py
+++ au2018forgecatalog/__init__.py
@@ -1,7 +1,20 @@
+import os
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
+    flc_tenant_id = os.environ.get('FLC_TENANT_ID')
+    settings['flc_tenant_id'] = flc_tenant_id
+    flc_user_id = os.environ.get('FLC_USER_ID')
+    settings['flc_user_id'] = flc_user_id
+    flc_password = os.environ.get('FLC_PASSWORD')
+    settings['flc_password'] = flc_password
+    forge_client = os.environ.get('FORGE_CLIENT')
+    settings['forge_client'] = forge_client
+    forge_secret = os.environ.get('FORGE_SECRET')
+    settings['forge_secret'] = forge_secret
+    forge_bucket_key = os.environ.get('FORGE_BUCKET_KEY')
+    settings['forge_bucket_key'] = forge_bucket_key
     """ This function returns a Pyramid WSGI application.
     """
     with Configurator(settings=settings) as config:
